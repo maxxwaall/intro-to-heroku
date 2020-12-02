48a96c3e47f68e443c19682bd9702e660fc09789
Fabiano Albino Ferreira <fabiano@kolekto.com.br>
2020-12-02 00:32:00 -0300

    add another database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join('www', 'build')));
 app.use(bodyParser.json());
 
 
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/dreamhouse';
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/d8cfhd00p762u9';
 
 if (process.env.DATABASE_URL !== undefined) {
   pg.defaults.ssl = true;
